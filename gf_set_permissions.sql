9894ec0816eb3bf83b46faed3d0967cdc365504d
Brad Boyle <ojalaquellueva@gmail.com>
2022-07-21 15:30:04 -0700

    update gf_set_permissions.sql

--- gf_set_permissions.sql
+++ gf_set_permissions.sql
@@ -7,12 +7,19 @@
 --		then copy/paste to execute the commands.
 -- ---------------------------------------------------------------------------
 
+-- New owner of growth forms tables
+\set new_owner bien
+
 -- Database containing growth form schema
 \set db_gf vegbien
 -- Growth form schema
 \set sch_gf growthforms
--- New owner of growth forms tables
-\set new_owner bien
+
+-- Analytical database
+\set db_adb vegbien
+-- Analytical schema
+\set sch_adb analytical_db
+
 
 \c :db_gf
 set search_path to :sch_gf;
@@ -23,4 +30,10 @@ ALTER SCHEMA :sch_gf OWNER TO :new_owner;
 SELECT 'ALTER TABLE ' || sch.table_name || ' OWNER TO ' || :'new_owner' || ';' 
 FROM information_schema.tables sch
 WHERE table_schema = :'sch_gf';
+
+-- Also change ownership of species_growth_forms table in analytical_db;
+\c :db_adb
+set search_path to :sch_adb;
+ALTER TABLE :sch_adb.species_growth_forms OWNER TO :new_owner;
+
 
